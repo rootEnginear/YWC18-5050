5cbecc885e661d8f1f1cae8c858f01ffe73fe1ba
Suthep Chanchuphol <suthep.chanchuphol@outlook.com>
2020-11-18 05:27:26 +0700

    fix: Assign production `assetPrefix`

--- next.config.js
+++ next.config.js
@@ -2,4 +2,5 @@ const isProduction = process.env.NODE_ENV === "production";
 
 module.exports = {
   basePath: isProduction ? "/YWC18-5050" : "",
+  assetPrefix: isProduction ? "/YWC18-5050" : "",
 };
