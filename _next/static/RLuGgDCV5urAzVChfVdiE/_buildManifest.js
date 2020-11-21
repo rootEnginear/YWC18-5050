42c1025d8e209b2d61a5414c6843ef7917bc279c
rootEnginear <suthep.chanchuphol@outlook.com>
2020-11-21 04:59:12 +0000

    Update main to output generated at 1443459

--- _next/static/RLuGgDCV5urAzVChfVdiE/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-a68472b3aff1853ce7c0.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-3834583bd75ba06a2470.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
